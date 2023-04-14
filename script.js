b59221918fe7a7c86913f56c53375831df0075b5
Andre <123985988+derre3@users.noreply.github.com>
2023-04-14 16:15:56 -0300

    remove leftover comment

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// let gridSize = 16;
-// const gridContainer = document.querySelector(".grid-container");
-
-// for (let i = 0; i < gridSize; i++) {
-//     const gridRow = document.createElement("div");
-//     gridRow.classList.add("grid-row");
-//     for (let j = 0; j < gridSize; j++) {
-//         const gridItem = document.createElement("div");
-//         gridItem.classList.add("grid-item");
-//         gridRow.appendChild(gridItem);
-
-//         gridItem.style.backgroundColor = "#c4c4c4"
-//         gridItem.style.border = "solid"
-//         gridItem.style.borderWidth = "1px"
-//         gridItem.style.borderColor = "#d8d8d8"
-//         gridItem.style.aspectRatio = "1 / 1"
-//     }
-//     gridContainer.appendChild(gridRow);
-//     gridRow.style.flex = "1 1 auto"
-// }
-
 window.onload = updateGrid(16)
 
 function updateGrid(gridSize) {
