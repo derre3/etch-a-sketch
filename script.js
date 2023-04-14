f2ac33db65948318cd523443f64fe9a64396e02c
Andre <123985988+derre3@users.noreply.github.com>
2023-04-14 12:56:11 -0300

    add basic styling to the grid

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ for (let i = 0; i < gridSize; i++) {
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
         gridRow.appendChild(gridItem);
+
+        gridItem.style.backgroundColor = "#c4c4c4"
+        gridItem.style.border = "solid"
+        gridItem.style.borderWidth = "1px"
+        gridItem.style.borderColor = "#d8d8d8"
+        gridItem.style.aspectRatio = "1 / 1"
     }
     gridContainer.appendChild(gridRow);
+    gridRow.style.flex = "1 1 auto"
 }
+
