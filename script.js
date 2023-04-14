1e43ebbc34ed294a78ea6b860aa81da72d2c1a29
Andre <123985988+derre3@users.noreply.github.com>
2023-04-14 19:00:05 -0300

    split main function into helper functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-window.onload = updateGrid(16)
+window.onload = updateGridSize(16)
 
-function updateGrid(gridSize) {
+function updateGridSize(gridSize) {
+    removeGrid();
     const gridContainer = document.querySelector(".grid-container");
     for (let i = 0; i < gridSize; i++) {
         const gridRow = document.createElement("div");
@@ -9,14 +10,63 @@ function updateGrid(gridSize) {
             const gridItem = document.createElement("div");
             gridItem.classList.add("grid-item");
             gridRow.appendChild(gridItem);
-    
-            gridItem.style.backgroundColor = "#d8d8d8"
-            gridItem.style.border = "solid"
-            gridItem.style.borderWidth = "1px"
-            gridItem.style.borderColor = "#c4c4c4"
-            gridItem.style.aspectRatio = "1 / 1"
+            gridItem.style.backgroundColor = "#d8d8d8";
+            gridItem.style.border = "solid";
+            gridItem.style.borderWidth = "1px";
+            gridItem.style.borderColor = "#c4c4c4";
+            gridItem.style.aspectRatio = "1 / 1";
         }
         gridContainer.appendChild(gridRow);
         gridRow.style.flex = "1 1 auto"
+    }
+    useColor();
+}
+
+function removeGrid() {
+    const gridItem = document.querySelectorAll(".grid-item");
+    const gridRow = document.querySelectorAll(".grid-row");
+    for (let i = 0; i < gridRow.length; i++) {
+        for (let j = 0; j < gridItem.length; j++) {
+            gridItem[j].remove();
+        }
+        gridRow[i].remove();
+    }
+}
+
+function useColor() {
+    const gridItem = document.querySelectorAll(".grid-item");
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener('mouseover', ()=> {
+            gridItem[i].style.backgroundColor = "#000000";
+            gridItem[i].style.borderColor = "#333333";
+        })
+    }
+}
+
+function useEraser() {
+    const gridItem = document.querySelectorAll(".grid-item");
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener('mouseover', ()=> {
+            gridItem[i].style.backgroundColor = "#d8d8d8";
+            gridItem[i].style.borderColor = "#c4c4c4"
+        })
+    }
+}
+
+function cleanGrid() {
+    const gridItem = document.querySelectorAll(".grid-item");
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].style.backgroundColor = "#d8d8d8";
+        gridItem[i].style.borderColor = "#c4c4c4";
+    }
+}
+
+function updateGridColor(color) {
+    const gridItem = document.querySelectorAll(".grid-item");
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener('mouseover', ()=> {
+            gridItem[i].style.backgroundColor = color;
+            gridItem[i].style.borderColor = "#333333";
+        })
     }
 }
